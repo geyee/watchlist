66455b4a92462c01df65d2e5a9e06fea62518e30
geyee <geyee@foxmail.com>
2024-10-09 00:22:36 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
+import os
+import sys
+
+import click
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask import url_for
 from markupsafe import escape
 
 
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
 
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
 
-name = 'Grey Li'
-movies = [
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    name = 'Grey Li'
+    movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
@@ -19,12 +48,34 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model): #表名将是 movie
+    id = db.Column(db.Integer, primary_key=True) #主键
+    title = db.Column(db.String(60)) #电影标题
+    year = db.Column(db.String(4)) #电影年份
+
 
 @app.route('/')
 #@app.route('/index')
 #@app.route('/home')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+
+    return render_template('index.html',user=user,movies=movies)
 # def hello():
 #     #return 'Welcome to My Watchlist! 欢迎来到我的 Watchlist!'
 #     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
